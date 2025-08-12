7d76d6228c1e475262e5d5a19081f828d59d41e3
NahusenayTadesse <124673344+NahusenayTadesse@users.noreply.github.com>
2025-08-12 22:30:02 +0300

    Another safe build for production. Dashboard mobile bottom navigation added, still needs some work.

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     let { data, children } = $props();
     import {page} from '$app/state';
-      import { MessageSquare, Inbox, NotebookPen, Megaphone, LogOut } from 'lucide-svelte';
+      import { MessageSquare, Inbox, NotebookPen, Megaphone, LogOut, X, AlignJustify, House } from 'lucide-svelte';
 
 
     import { LayoutDashboard, Quote, MessageSquareQuote } from '@lucide/svelte';
 	import { enhance } from '$app/forms';
+	import { slide } from 'svelte/transition';
 
 let currentPage = $state(page.url.pathname.charAt(1).toUpperCase() + page.url.pathname.slice(2));
 
@@ -16,7 +17,6 @@ let currentPage = $state(page.url.pathname.charAt(1).toUpperCase() + page.url.pa
     { name: 'Messages', href: '/dashboard/messages', icon: MessageSquareQuote },
     { name: 'Blogs', href: '/dashboard/blogs', icon: NotebookPen },
     { name: 'Annoucement', href: '/dashboard/announcement', icon: Megaphone }
-   
   ];
     let sidebar = $state(false);
     let submitting = $state(false);
@@ -24,10 +24,14 @@ let currentPage = $state(page.url.pathname.charAt(1).toUpperCase() + page.url.pa
     function onsubmit() {
         submitting = true;
     }
+  
+    let open = $state(false);
+        let  Menuicon = $derived (open ? X : AlignJustify);
+
 
 </script>
 
-<div class="flex h-screen">
+<div class="lg:flex h-screen hidden">
   <!-- Sidebar -->
   <aside class="{sidebar ? 'w-[250px]': 'w-[70px]'} 
   {sidebar ? 'min-w-[250px]': 'min-w-[70px]'} 
@@ -84,7 +88,7 @@ let currentPage = $state(page.url.pathname.charAt(1).toUpperCase() + page.url.pa
       <p class="text-sm text-gray-500">Unreplied Quotes</p>
       <p class="text-2xl font-semibold text-gray-900">{data?.unrepliedCount.length - data?.replyCount.length}</p>
 
-      <form method="post" action="?/logout" use:enhance {onsubmit}>
+      <form method="post" action="/dashboard/?/logout" use:enhance {onsubmit} id="logoutForm">
     <button type="submit" name="logout" class="absolute right-4 top-4 flex flex-row gap-2 items-center justify-center" disabled={submitting}>
          <LogOut class="{submitting ? 'animate-spin': ''} w-4 h-4" />
         {submitting ? "Logging out..." : "Logout"}
@@ -94,13 +98,47 @@ let currentPage = $state(page.url.pathname.charAt(1).toUpperCase() + page.url.pa
   
   </header>
        <div class="p-8">
-            {@render children()}</div>
+            </div>
     </main>
 </div>
 
+<section class="lg:hidden flex flex-col p-0">
+  <button onclick={()=>open = !open} class="fixed top-2 left-4"><Menuicon /></button>
+  {#if open} 
+
+  <form method="post" action="/dashboard/?/logout" class="fixed left-4 top-12 p-2 z-100 bg-white w-1/2" use:enhance {onsubmit} transition:slide|global>
+        <a href="/" class="flex flex-row gap-2"><House class="w-4 h-4" /> Home</a>
+
+    <button type="submit" name="logout" class="flex flex-row gap-2 items-center justify-center" disabled={submitting}>
+         <LogOut class="{submitting ? 'animate-spin': ''} w-4 h-4" />
+        {submitting ? "Logging out..." : "Logout"}
+    </button>
+</form>
+{/if}
+   
+  <div class="pt-16 py-24 px-4">
+  
+   {@render children()}
+   </div>
+
+<div class="grid lg:hidden grid-cols-5 gap-2 fixed bottom-0 z-100 w-full bg-light-blue-4 justify-center items-center align-center p-2">
 
 
+  {#each navItems as item}
+
+    <a href={item.href} class="flex flex-col gap-1 text-white justify-center items-center">
+   
+    <item.icon size="18" />
+    <p class="text-[10px] tex-white">{item.name}</p>
+    
+
+    </a>
+  {/each}
+  
+</div>
+
+</section>
  
 
-    
+   
 
